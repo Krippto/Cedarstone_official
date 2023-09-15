1ac3c76cfadc448a1340529120a60648a72f3950
Krippto <136462177+Krippto@users.noreply.github.com>
2023-09-15 12:24:49 +0530

    Add files via upload
    Updated all wireframes

---
+++ index.js
@@ -0,0 +1,7 @@
+function goRight() {
+    document.querySelector(".carousel-container").scrollLeft += 1000;
+}
+
+function goLeft() {
+    document.querySelector(".carousel-container").scrollLeft -= 1000;
+ }
